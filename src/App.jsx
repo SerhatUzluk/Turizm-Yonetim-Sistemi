21883e7a9147038fba6aacd67b7c307d54b36679
Ridvan Korurer <79053825+MRidvanKorurer@users.noreply.github.com>
2024-04-06 23:58:31 +0300

    font eklendi - renkler eklendi - mainLayout yapısı kuruldu

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
+import MainLayout from "./layouts/MainLayout";
 
 function App() {  
-  console.log("deneme");
+  console.log("deneme2");
 
   return (
     <>      
+    <MainLayout>
+      {/* font kullanımı */}
+      <div className="font-gemunu text-lightBlue">
+        deneme
+      </div>
+    </MainLayout>
       
     </>
   )
