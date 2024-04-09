1d43705cc6dc82e9bdb456e3438ae22a58f63cdf
Serhat Uzluk <132030068@ogr.uludag.edu.tr>
2024-04-09 22:34:37 +0300

    Revert "Navbar bileşeni oluşturuldu, main.jsx'te router tanımlandı. Eksik olan paketler eklendi."
    This reverts commit 84448293f3eef92524befd49d7890bc87b4e1b9a.

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-{/*Route oluşturmak için router oluşturuldu.*/}
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App/>,
-    errorElement: <></>
-}
-])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <App />
   </React.StrictMode>,
 )
