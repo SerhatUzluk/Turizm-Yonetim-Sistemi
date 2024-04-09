d962a255075b1748a6e8c0be4989911641b05599
Mustafa Gökalp <gokalpmustafa49@gmail.com>
2024-04-09 22:49:45 +0300

    "footer kısmı tamamlandı"

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import MainLayout from "./layouts/MainLayout";
 
 function App() {  
+  console.log("deneme2");
 
   return (
     <>      
     <MainLayout>
-     
+      {/* font kullanımı */}
       <div className="font-gemunu text-lightBlue">
-       
+        deneme
       </div>
     </MainLayout>
       
