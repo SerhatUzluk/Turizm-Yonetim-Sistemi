83ec0a58103266abbdb95bc6a2d38fed415b2574
Mustafa Gökalp <gokalpmustafa49@gmail.com>
2024-04-09 17:16:53 +0300

    "Footer alanı tamamlandı, component klasörü oluşturulup içine Maps dosyası oluşturuldu"

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import MainLayout from "./layouts/MainLayout";
 
 function App() {  
-  console.log("deneme2");
 
   return (
     <>      
     <MainLayout>
-      {/* font kullanımı */}
+     
       <div className="font-gemunu text-lightBlue">
-        deneme
+       
       </div>
     </MainLayout>
       
