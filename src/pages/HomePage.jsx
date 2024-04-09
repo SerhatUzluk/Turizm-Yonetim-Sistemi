2207aa2a6e0f3a7505f12bb1b4a8a9f84089e427
Ridvan Korurer <79053825+MRidvanKorurer@users.noreply.github.com>
2024-04-09 21:27:23 +0300

    Reapply "Homepage slider-heroSection yapıldı"
    This reverts commit 46ddda3ebaf57ef22a40a4fb2940b2d236a24a15.

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Slider from '../components/Slider/Slider'
+import HeroSection from '../components/HeroSection/HeroSection'
+
+
+const HomePage = () => {
+  return (
+    <div>
+        <Slider />
+        <HeroSection />
+    </div>
+  )
+}
+
+export default HomePage
