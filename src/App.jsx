7e42dc8c718dba60a5f6b75126854a66d6947725
Mustafa Gökalp <gokalpmustafa49@gmail.com>
2024-04-10 16:10:10 +0300

    deneme

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,13 @@ function App() {
   return (
     <>                
     <MainLayout>
-<<<<<<< HEAD
+
       {/* font kullanımı */}
       <div className="font-gemunu text-lightBlue">
         deneme
       </div>
-=======
 
->>>>>>> 93222d3840d0fbbaae5472969bf07f2c6c669ea2
+
     </MainLayout>
       
     </>
