76c430c157b70b4d113ca2fd6c0a1a8ef84e1a1d
Serhat Uzluk <132030068@ogr.uludag.edu.tr>
2024-04-23 18:13:45 +0300

    BrandInformation dosyası oluşturuldu içine örnek veriler girildi, BrandCard bileşeni oluşturuldu ve anasayfa'ya eklendi.

---
+++ src/components/BrandCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export const BrandCard = ({item}) => {  
+  return (
+    <>
+      <div className="flex flex-wrap bg-light hover:scale-105 transition duration-300">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg flex-1 flex-row p-4 m-2 bg-white">
+          <img
+            className="w-full aspect-ratio-4/3"
+            src={item.logo}
+            alt={item.name + 'logo'}
+          />          
+          <div className="px-6 py-4 font-gemunu bg-lightBlue">
+            <div className="font-bold text-xl mb-2">{item.name}</div>
+            <p className="text-gray-700 text-base">
+              {item.description}
+            </p>
+          </div>                    
+        </div>
+      </div>
+    </>
+  );
+};
