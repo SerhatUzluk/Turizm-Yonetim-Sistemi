7fa456b238491831af88dd4466d65b30b7ded9a3
Ridvan Korurer <79053825+MRidvanKorurer@users.noreply.github.com>
2024-04-24 14:55:56 +0300

    heroSection düzenlendi

--- src/components/HeroSection/HeroSectionHeader.jsx
+++ src/components/HeroSection/HeroSectionHeader.jsx
@@ -2,23 +2,17 @@ import React from "react";
 
 const HeroSectionHeader = () => {
   return (
-    <>
+    <div className="flex justify-center items-center flex-col gap-5 p-4  rounded-xl my-8 shadow-xl">
       <div>
-        <h2 className=" font-gemunu text-6xl font-bold">Lorem, ipsum dolor.</h2>
+        <h2 className=" font-gemunu text-6xl font-bold">SVR ile Güvenli Yolculuk</h2>
       </div>
 
-      <div className=" text-center font-light w-1/2">
+      <div className=" text-center font-semibold w-1/2">
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores
-          earum nulla magnam odit, perspiciatis sunt, laboriosam quas magni amet
-          mollitia dolore? Voluptates dignissimos quae similique id saepe illum
-          error et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
-          laborum totam sint illum rerum fugiat magnam, voluptas veniam autem
-          aperiam similique hic molestias a ratione explicabo placeat ut
-          temporibus repellat.
+          Yeni nesil seyahat deneyimi için tasarlanmış platformumuz, kullanıcılarına kolaylık, güvenilirlik ve esneklik sunuyor. Her türden seyahat ihtiyacınızı karşılamak için geniş bir seçenek yelpazesi sunarken, kişiselleştirilmiş önerilerle size en uygun seçenekleri sunuyoruz. Güvenli ödeme sistemimiz ve 7/24 müşteri desteği ile seyahat planlarınızı kolayca gerçekleştirebilir, unutulmaz anlar yaşamak için yeni maceralara yelken açabilirsiniz. Hayalinizdeki seyahati keşfetmek için bize katılın!
         </p>
       </div>
-    </>
+    </div>
   );
 };
 
