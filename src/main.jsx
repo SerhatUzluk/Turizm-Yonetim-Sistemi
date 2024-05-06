551a77f4c5e8b2173e34aeb736f24cc5f4decc41
Ridvan Korurer <79053825+MRidvanKorurer@users.noreply.github.com>
2024-05-06 23:39:47 +0300

    main routelar düzenlendi

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,8 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 import LoginPage from './pages/LoginPage.jsx'
 import ResetPassword from './pages/ResetPassword.jsx'
+import TicketPage from "./pages/TicketPage.jsx";
+import PaymentPage from "./pages/PaymentPage.jsx";
 
 {
   /*Route oluşturmak için router oluşturuldu.*/
@@ -51,6 +53,16 @@ const router = createBrowserRouter([
   {
     path: '/sifre-yenile',
     element: <ResetPassword/>,
+    errorElement:<></>
+  },
+  {
+    path: '/bilet-sorgu',
+    element: <TicketPage/>,
+    errorElement:<></>
+  },
+  {
+    path: '/odeme-sistemi',
+    element: <PaymentPage />,
     errorElement:<></>
   }
 
