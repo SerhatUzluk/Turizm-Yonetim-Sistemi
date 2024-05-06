58cc5b4ffef4b0c8ee72bdbbc33dc06d90be658b
Serhat Uzluk <132030068@ogr.uludag.edu.tr>
2024-05-06 22:21:18 +0300

    Revert "Navbar' a search bölümü eklendi, BusTemplate2 'de renk düzenlemeleri yapıldı ve cinsiyet seçme butonu eklendi. CampaignsPage bileşeni oluşturuldu."
    This reverts commit 1223408ba08b38f9e7ddd0cee3a2e8cc6b69815c.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import SideBar from '../components/SideBar/SideBar'
 import { sideBarReducer } from './slices/SideBarSlice'
-import  { campaignReducer }  from './slices/CampaignSlice'
+
 export const store = configureStore({
   reducer: {
-    sideBar: sideBarReducer,   
-    campaignDetail: campaignReducer 
-  },  
+    sideBar: sideBarReducer
+  },
 })
 
